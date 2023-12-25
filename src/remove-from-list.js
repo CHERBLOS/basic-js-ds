ba9b06ba57d499ed344bf32811cf74dede897428
Vladislav Y. <vlad.xxxx4@gmail.com>
2023-12-25 13:18:11 +0300

    feat: solve 3 tasks

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l) return null;
+  
+  while (l) {
+    if (k === l.value) {
+      l = l.next;
+      return removeKFromList(l, k);
+    } else {
+      l.next = removeKFromList(l.next, k);
+      return l;
+    }
+  } 
 }
 
 module.exports = {
